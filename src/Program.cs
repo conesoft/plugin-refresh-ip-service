0b85cc95d33da654aa6e2d57028e0b251669b50d
David Spörri <davepermen@hotmail.com>
2025-01-02 20:56:54 +0100

    updated for new host

--- src/Program.cs
+++ src/Program.cs
@@ -1,51 +1,70 @@
 using Conesoft.Files;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Conesoft.Hosting;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Conesoft.Services.RefreshIP
+var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args);
+builder.Services
+    .AddPeriodicGarbageCollection(TimeSpan.FromMinutes(5));
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var configuration = new ConfigurationBuilder().AddJsonFile(Host.GlobalConfiguration.Path).Build();
+
+var file = Host.GlobalStorage / "FromSources" / "Ipify" / Filename.From("Ip", "txt");
+file.Parent.Create();
+
+var ipify = new Conesoft.Ipify.Client(new HttpClient());
+var dnsimple = new Conesoft.DNSimple.Client(new HttpClient());
+
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+do
 {
-    public class Program
+    try
     {
-        public static void Main(string[] args)
+        var lastIp = file.Exists ? IPAddress.Parse(await file.ReadText()) : IPAddress.None;
+
+        var currentIp = await ipify.GetPublicIPAddress();
+
+        if (currentIp.ToString() != lastIp.ToString()) // urgh, value comparison, the cheap way
         {
-            try
-            {
-                var log = Hosting.Host.Root / Filename.From(Hosting.Host.Name.ToLowerInvariant() + " log", "txt");
-
-                System.IO.File.WriteAllText(log.Path, "");
-
-                Log.Logger = new LoggerConfiguration()
-                    .WriteTo.File(log.Path, buffered: false, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(1))
-                    .WriteTo.Console()
-                    .CreateLogger();
-            }
-            catch (Exception)
-            {
-                Log.Logger = new LoggerConfiguration()
-                    .WriteTo.Console()
-                    .CreateLogger();
-            }
-
-            Log.Information("App has started");
-
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            finally
-            {
-                Log.Information("App has stopped");
-            }
+            Log.Information("updating IP Address to {new}", currentIp);
+            await UpdateDnsRecord(currentIp);
+            await file.WriteText(currentIp.ToString());
         }
+    }
+    catch (Exception e)
+    {
+        Log.Error("error: {exception}", e.Message);
+    }
+}
+while (await timer.WaitForNextTickAsync());
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                })
-                .UseSerilog();
+    async Task UpdateDnsRecord(IPAddress address)
+{
+    dnsimple.UseToken(configuration["hosting:dnsimple-token"]);
+
+    var account = (await dnsimple.GetAccounts()).First();
+
+    var zones = await account.GetZones();
+
+    foreach (var zone in zones)
+    {
+
+        var hosted = Host.Root / "Deployments" / "Websites" / Filename.From(zone.Name, "zip");
+        if (hosted.Exists)
+        {
+            Log.Information("Updating Zone {zone}", zone.Name);
+            var record = await zone.GetRecord(Conesoft.DNSimple.RecordType.A);
+            await record.Update(address.ToString());
+        }
     }
 }
